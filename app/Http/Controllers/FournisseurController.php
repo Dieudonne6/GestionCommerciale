f9b1ff0cad678cc39d0807167ebf4896a792cd24
Pascaline Dah Zinsou <pascaline@wazoom-studio.com>
2024-11-21 12:48:21 +0100

    made some change.

---
+++ app/Http/Controllers/FournisseurController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FournisseurController extends Controller
+{
+    public function fournisseur(){
+        return view('pages.fournisseur');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FournisseurController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/fournisseur', [FournisseurController::class, 'fournisseur']);
+
