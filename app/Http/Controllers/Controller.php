195c70c9b3cd39e221f0e6d0f69411cb2f00bb22
cboxone <logossoumichel@gmail.com>
2024-11-27 15:22:52 +0100

    fd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -21,11 +21,17 @@ class Controller extends BaseController
 
     public function store(Request $request)
     {
+        // Validation avec gestion des doublons
         $request->validate([
             'codeCais' => 'required|string|unique:caises,codeCais|max:255',
             'libelleCais' => 'required|string|max:255',
+        ], [
+            'codeCais.required' => 'Le code de la caisse est obligatoire.',
+            'codeCais.unique' => 'Ce code existe déjà. Veuillez en saisir un autre.',
+            'libelleCais.required' => 'Le libellé de la caisse est obligatoire.',
         ]);
 
+        // Création de la caisse
         Caise::create([
             'codeCais' => $request->codeCais,
             'libelleCais' => $request->libelleCais,
@@ -36,11 +42,17 @@ class Controller extends BaseController
 
     public function update(Request $request, $id)
     {
+        // Validation avec gestion des doublons lors de la modification
         $request->validate([
             'codeCais' => 'required|string|max:255|unique:caises,codeCais,' . $id . ',idCais',
             'libelleCais' => 'required|string|max:255',
+        ], [
+            'codeCais.required' => 'Le code de la caisse est obligatoire.',
+            'codeCais.unique' => 'Ce code existe déjà pour une autre caisse.',
+            'libelleCais.required' => 'Le libellé de la caisse est obligatoire.',
         ]);
 
+        // Mise à jour de la caisse
         $caisse = Caise::findOrFail($id);
         $caisse->update([
             'codeCais' => $request->codeCais,
@@ -52,6 +64,7 @@ class Controller extends BaseController
 
     public function destroy($id)
     {
+        // Suppression de la caisse
         $caisse = Caise::findOrFail($id);
         $caisse->delete();
 
